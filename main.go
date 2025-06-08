c078ca9e40167526a81304bb1f7dc2bfbbbc63c2
aleBranching <105317926+aleBranching@users.noreply.github.com>
2025-06-08 13:10:49 +0100

    Merge pull request #2 from aleBranching/update_branch
    unnecessary i++

--- main.go
+++ main.go
@@ -200,19 +200,16 @@ func getConfig() config {
 	for i, arg := range os.Args[1:] {
 		if arg == "--node-id" {
 			cfg.id = os.Args[i+2]
-			i++
 			continue
 		}
 
 		if arg == "--http-port" {
 			cfg.httpPort = os.Args[i+2]
-			i++
 			continue
 		}
 
 		if arg == "--raft-port" {
 			cfg.raftPort = os.Args[i+2]
-			i++
 			continue
 		}
 	}
